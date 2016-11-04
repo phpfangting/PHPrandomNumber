87747ed53f7891dd3ea1957fd19c419862fcb12b
liufangting <liufangting@epailive.com>
2016-11-04 19:13:18 +0800

    yes

---
+++ php/js/uploadFile.js
@@ -0,0 +1,55 @@
+/**
+ * Created by liufangting on 2016/11/4.
+ */
+
+
+function UploadFile() {
+
+    this.options = {
+        url: '',//请求的url地址 [required]
+        formId: '',//要提交的表单id [required]
+        fileId: '',//文件上传input对应的id值 [required]
+        iframeId: '',//新窗口对应的id值 [required]
+        iframeName: '',//新窗口对应的名称 [required]
+        imgId: ''//新窗口对应的名称 [required]
+    };
+    this.formObj = {};//表单对象
+    this.fileObj = {};//文件对象
+    this.iframeObj = {};//子窗口对象
+    this.init = function (param) {
+        for (k in param) {
+            this.options[k] = param[k];
+        }
+        this.formObj = document.getElementById(this.options['formId']);
+        this.fileObj = document.getElementById(this.options['fileId']);
+        this.iframeObj = document.getElementById(this.options['iframeId']);
+        this.imgObj = document.getElementById(this.options['imgId']);
+        if (!this.formObj || !this.fileObj || !this.iframeObj) {
+            console.log('error');
+            return false;
+        }
+        _uploadThis = this;
+    }
+    this.submitForm = function () {
+        this.fileObj.onchange = function () {
+            _uploadThis.formObj.submit();
+        };
+    }
+
+    this.listenEvent = function () {
+        this.iframeObj.attachEvent ? this.iframeObj.attachEvent('onload', this.returnData) : this.iframeObj.onload = this.returnData;
+    }
+
+    this.returnData = function () {
+        var acceptData = JSON.parse(_uploadThis.iframeObj.contentWindow.document.body.innerHTML);
+    
+        if (acceptData.data['url']) {
+            _uploadThis.imgObj.setAttribute('src', acceptData.data['url']);
+        }
+    }
+    this.upload = function (param) {
+        this.init(param);
+        this.submitForm();
+        this.listenEvent();
+    }
+}
--- php/upload.php
+++ php/upload.php
@@ -8,24 +8,26 @@
 
 
 $imgRecource = $_FILES['upload_file'];
-if(!empty($imgRecource['tmp_name'])){
-    $targetDir = __DIR__.'/'.'uploads/';
-    if(!is_dir($targetDir)){
-        mkdir($targetDir,0777);
+if (!empty($imgRecource['tmp_name'])) {
+    $targetDir = __DIR__ . '/' . 'uploads/';
+    if (!is_dir($targetDir)) {
+        mkdir($targetDir, 0777);
     }
-    $fileName = uniqid('epailive').time().'.jpg';
-    $targetFileNamePath = $targetDir.$fileName;
+    $fileName = uniqid('epailive') . time() . '.jpg';
+    $targetFileNamePath = $targetDir . $fileName;
 
-    move_uploaded_file($imgRecource['tmp_name'],$targetFileNamePath);
+    move_uploaded_file($imgRecource['tmp_name'], $targetFileNamePath);
 
-    $str =<<<HTML
-    
-    <script type="text/javascript">
-        window.parent.document.getElementById('headimg').setAttribute('src',"/php/uploads/{$fileName}");
-    </script>
-HTML;
+//    $str =<<<HTML
+//
+//    <script type="text/javascript">
+//        window.parent.document.getElementById('headimg').setAttribute('src',"/php/uploads/{$fileName}");
+//    </script>
+//HTML;
 
-    echo $str;
+    $result = ['status' => 200, 'msg' => '', 'data' => ['url' => "/php/uploads/{$fileName}"]];
+
+    echo json_encode($result);
 }
 
 
