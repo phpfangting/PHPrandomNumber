b3a1949c5777e669f6d766ccac97829a7e8d782a
liufangting <liufangting@epailive.com>
2016-12-02 18:07:44 +0800

    update

---
+++ php/test22.php
@@ -0,0 +1,71 @@
+<?php 
+
+ $str = "
+dad6aa9 @{0}: commit: update
+0d5e6b1 @{6}: commit (merge): update
+4572b44 @{7}: commit: update
+7e79d39 @{11}: commit: 公司详情
+c868bb3 @{13}: commit: update
+33cf491 @{17}: commit: update
+2941a1b @{35}: commit: update
+33aa5d7 @{46}: commit: yes
+5b0e2f5 @{54}: commit (merge): update
+97f4968 @{55}: commit: yes
+c2b8a45 @{64}: commit: 拍品详情
+5db560d @{76}: commit: update
+6183e55 @{77}: commit: tdk
+6fc9940 @{78}: commit: tdk
+183b4bf @{79}: commit: tdk
+f730f90 @{83}: commit: tdk
+2ddfa67 @{89}: commit: update
+5bada0e @{90}: commit: update
+2a28aac @{99}: commit: update
+78788ec @{103}: commit: ys
+440f979 @{105}: commit: ys
+e1e0bdf @{113}: commit: ys
+0a45b5c @{114}: commit (merge): ys
+f04dafd @{115}: commit: ys
+f3f75e8 @{124}: commit (merge): 公司详情
+c6d298d @{125}: commit: 公司详情
+7560b68 @{128}: commit: 猜你喜欢
+98f599d @{129}: commit: 猜你喜欢
+a817abb @{130}: commit: update
+4bf892a @{138}: commit: update
+a36b58d @{140}: commit: update
+ea9a407 @{143}: commit (merge): update
+1de45b6 @{145}: commit: update
+fdfd758 @{148}: commit: update
+e99fbbc @{151}: commit (merge): update
+19eac5c @{153}: commit: update
+f7bb342 @{154}: commit (merge): update
+4b82d73 @{155}: commit: update
+19a75a0 @{159}: commit: update
+64b5db4 @{161}: commit: update
+c787319 @{164}: commit (merge): update
+bdbfbeb @{167}: commit: update
+157db13 @{168}: commit: update
+06d2784 @{170}: commit: update
+2db0820 @{171}: commit: update
+329a23c @{177}: commit: 公司介绍
+3b7ca5f @{179}: commit: upate
+2c51739 @{183}: commit (merge): update
+7b2f31a @{185}: commit: 公司详情
+f5ca7da @{187}: commit: update
+be27e4b @{190}: commit: update
+212f2de @{195}: commit (merge): update
+d011625 @{198}: commit: up
+b718a13 @{200}: commit: up
+926106a @{201}: commit: up
+c3b7f0e @{204}: commit (merge): ;
+0f5448f @{208}: commit (merge): upate
+d013403 @{209}: commit: update
+a0c7132 @{210}: commit: update
+ ";
+
+ // $pattern = "/(?<!@)[^\{]{[0-9]*\}/mi";
+
+ // preg_match_all($pattern,$str , $data);
+
+ // print_r($data);
+
+ echo time();
