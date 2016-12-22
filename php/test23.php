e1106c7e31b2991b6ceee51f9c8949bd9219d5b5
liufangting <liufangting@epailive.com>
2016-12-22 13:43:08 +0800

    update

--- php/test23.php
+++ php/test23.php
@@ -1,23 +1,28 @@
 <?php
 	$str = "http://www.phpweb.com/part/21995";
 
-	$a = 6.11;
-	$b = 6.11;
+// 	$a = 6.11;
+// 	$b = 6.11;
 
-	var_dump($a==$b);
+// 	var_dump($a==$b);
 
-	echo 'hello1';
-	echo 'sssdsdd';
-	echo 'one';
-	echo 'two';
+// 	echo 'hello1';
+// 	echo 'sssdsdd';
+// 	echo 'one';
+// 	echo 'two';
 
-    echo 'three';
-    echo 'five';
-    echo 'six';
+//     echo 'three';
+//     echo 'five';
+//     echo 'six';
 
-    echo '1';
+//     echo '1';
 
-echo '2';
+// echo '2';
 
-echo '3';
-echo '4';
+// echo '3';
+// echo '4';
+
+
+	$num = 7.4999;
+
+	echo bcadd($num, 0,2); 
