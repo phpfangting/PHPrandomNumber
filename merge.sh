511264a07cd1c079ccfb1c6e1ac190db136543c2
liufangting <liufangting@epailive.com>
2017-02-24 14:37:16 +0800

    update

--- merge.sh
+++ merge.sh
@@ -5,9 +5,9 @@
 #date  :2017-02-24
 
 UPDATE_BRANCH='' #需要更新操作的分支
-TARGET_BRANCH=$1 #目标分支
+TARGET_BRANCH=$2 #目标分支
 MERGE_BRANCH=`git branch|grep '*'|awk '{print $2}'` #要合并的分支
-COMMENT=$2 #注释
+COMMENT=$1 #注释
 
 COLOR_START_GREEN="\033[32m" #绿色
 COLOR_END="\033[0m"
@@ -71,13 +71,62 @@ function updateBranch(){
 	success;
 }
 
-UPDATE_BRANCH=$MERGE_BRANCH
-updateBranch;
+
+#更新代码
+
+if [ "$TARGET_BRANCH" = "" ];then
+
+	UPDATE_BRANCH=$MERGE_BRANCH
+	updateBranch;
+
+else
+	
+	#更新要合并的分支
+
+	UPDATE_BRANCH=$MERGE_BRANCH
+	updateBranch;
+
+	MSG="从本地  $UPDATE_BRANCH 分支切换到本地 $TARGET_BRANCH 分支"
+	success;
+	git checkout $TARGET_BRANCH
+	
+	#更新并提交目标分支
+	UPDATE_BRANCH=$TARGET_BRANCH
+	updateBranch;
+
+		
+	#在目标分支上合并分支 
+	MERGE_STATUS=`git merge $MERGE_BRANCH|grep 'failed'` #合并分支
+
+	if [ "$MERGE_STATUS" != "" ];then
+
+		MSG="合并分支出现错误,正尝试解决"
+		error;
+		git add .
+		git commit -am '解决合并分支错误'
+	fi
+	
+	#推送代码到目标分支
+	MSG="推送代码到远程 $TARGET_BRANCH 分支"
+	success;
+	git push origin $TARGET_BRANCH
+
+	#更新代码到目标分支
+	MSG="更新本地 $TARGET_BRANCH 分支"
+	success;
+	git pull origin $TARGET_BRANCH
+
+	MSG="在 $TARGET_BRANCH 分支 打版完成！！！"
+	success;
+        
+        #切换到被合并的分支OA
+	MSG="从 $TARGET_BRANCH 分支 切换到 $MERGE_BRANCH 分支"
+	success;
+	git checkout $MERGE_BRANCH
+	git pull origin $MERGE_BRANCH
+
+		    
 
 
 
-
-
-
-
-
+fi
