c360984e1fa00ee51a5f87b8cf8be90af912df56
liufangting <liufangting@epailive.com>
2017-03-06 14:09:15 +0800

    curl

---
+++ php/curl.php
@@ -0,0 +1,35 @@
+<?php
+
+
+	// $ch=curl_init();
+
+
+	// class A{
+
+	// 		private $name='zhangsan';
+	// 		public  $age=10;
+	// 		public  $result = ['name'=>'张三','age'=>17];	
+	// 		const DB=10;
+	// 		public function  say(){
+	// 			echo 'hello';
+	// 		}
+	// }	
+
+	// $a = new A();
+	// $re = new ReflectionClass('A');
+	// $result = $re->getProperties();
+	// var_dump($result);
+	// $reflections = ReflectionClass::export('A',true);
+	// $obj = new ReflectionClass('A');
+	//echo  $obj->getconstant('DB');
+
+
+	// $ch=curl_init();
+	// curl_setopt($ch, CURLOPT_URL, 'www.baidu.com');
+	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	// $result=curl_exec($ch);
+	// print_r($result);
+
+
+echo  base64_encode('\dss');
+// echo base64_decode($str);
