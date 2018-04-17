297a0ba7247d277039aeed486f95c446ab92bf29
echo music <liufangting@epailive.com>
2018-04-17 14:14:11 +0800

    update

--- jquery/js/Juqery1.js
+++ jquery/js/Juqery1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created by liufangting on 2016/11/8.
  */
@@ -12,18 +11,3 @@
 
     });
 })(jQuery);
-=======
-/**
- * Created by liufangting on 2016/11/8.
- */
-
-(function ($, undefined) {
-    $.fn.extend({
-        addCss: function () {
-            this.css({background:'red'});
-        }
-
-
-    });
-})(jQuery);
->>>>>>> ade65abf0e33ef7c43391325c8e8637a51859c00
