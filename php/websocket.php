5543aaf81acb88c89e8510956143b6527f9a60d2
echo music <liufangting@epailive.com>
2018-04-20 17:22:27 +0800

    update

--- php/websocket.php
+++ php/websocket.php
@@ -3,9 +3,10 @@
 $to_uid = 789;
 // 推送的url地址，使用自己的服务器地址
 $push_api_url = "http://47.95.250.84:2121/";
+
 $post_data = array(
    "type" => "publish",
-   "content" => "你已经让我失望一次了".uniqid('me'),
+   "content" =>'往事如风',
    "to" => $to_uid, 
 );
 $ch = curl_init ();
@@ -18,3 +19,4 @@ curl_setopt ($ch, CURLOPT_HTTPHEADER, array("Expect:"));
 $return = curl_exec ( $ch );
 curl_close ( $ch );
 var_export($return);
+
