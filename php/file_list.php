014d6cc4abbfea71e88d16856243d0b6bd2cadc7
echo music <liufangting@epailive.com>
2018-05-24 14:40:29 +0800

    update

--- php/file_list.php
+++ php/file_list.php
@@ -7,8 +7,6 @@ function  file_list($dir){
 
 		if(is_dir($dir)){
 			$list_dir = scandir($dir);
-
-
 			foreach ($list_dir as $key => $file) {
 				if($file == '.' || $file == '..'){
 					continue;
@@ -27,4 +25,9 @@ function  file_list($dir){
 
 $rs = file_list('./phpcore/');
 
-print_r($rs);
+// print_r($rs);
+
+$str="eyJpdiI6IlRMTXpDVkxoUjB1M2ZcL2VaTjBKcnVnPT0iLCJ2YWx1ZSI6IitmVDdQanEwN3RkK01ORmxKZDhxVXB4WXZ0M3p4MEVUbmRkdEE0aFA2VXhVTGhuWFMwekd2cDdIQjBLaGQ4NTdPK3AxSTYzZVFlbkI0NmZNaEw0anN3PT0iLCJtYWMiOiI4NTc5ODhjNTUwMDRlMTEzZDgzMDgxM2UyMmQ4M2M5NGQxMWQwNmQ4NGE0ZTRmMzZiNWMzNTM5Mjc4NzFkYzU5In0%3D";
+
+
+echo base64_decode($str);
